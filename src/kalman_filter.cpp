548d9fee452bbc05d5de86290b015e516450ccd0
ashupatel10@gmail.com <ashupatel10@gmail.com>
2019-05-18 09:08:31 +0000

    Extended Kalman Filter project

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,7 +1,10 @@
 #include "kalman_filter.h"
+#include <iostream>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+using std::cout;
+using std::endl;
 
 /* 
  * Please note that the Eigen library does not initialize 
@@ -26,16 +29,76 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+  // Convert Cartesian to Polar conrdinates
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+  
+  float c1 = sqrt(px*px+py*py);
+
+  // check division by zero
+  if (c1 < 0.0001) {
+    cout << "Error - Division by Zero" << endl;
+    px += 0.001;
+    px += 0.001;
+    c1 = sqrt(px*px+py*py);
+  }
+  
+  float c2 = atan2(py,px);
+  float c3 = (px*vx + py*vy)/c1;
+  
+  VectorXd h_func_(3);
+  h_func_ << c1, c2, c3;
+ 
+  VectorXd y = z - h_func_;
+  
+  // Normalize phi
+  while(y(1) > M_PI) {
+    y(1) -= 2 *M_PI;
+  }
+  
+  while(y(1) < -M_PI) {
+    y(1) += 2 *M_PI;
+  }
+  
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
